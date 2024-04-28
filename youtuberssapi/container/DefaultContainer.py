393e367cabd2fda9a8b7aa65af314ce2f7f3db62
Michele Capichioni <capimichi@gmail.com>
2024-04-28 20:42:55 +0200

    Added RssHelper dependency to DefaultContainer and updated FeedService initialization with it. Added get_host and get_port methods for base URL construction.

--- youtuberssapi/container/DefaultContainer.py
+++ youtuberssapi/container/DefaultContainer.py
@@ -1,4 +1,8 @@
+from os import environ
+
+from youtuberssapi.helper.RssHelper import RssHelper
 from youtuberssapi.service.FeedService import FeedService
+from youtuberssapi.service.VideoService import VideoService
 
 
 class DefaultContainer:
@@ -7,7 +11,15 @@ class DefaultContainer:
     classes = {}
 
     def __init__(self):
-        self.set('FeedService', FeedService())
+
+        self.set('RssHelper', RssHelper())
+
+        self.set('FeedService', FeedService(
+            rss_helper=self.get('RssHelper'),
+            base_url=self.get_base_url()
+        ))
+
+        self.set('VideoService', VideoService())
 
     def get_instance(self):
         if self.instance is None:
@@ -22,3 +34,12 @@ class DefaultContainer:
     def set (self, class_name, class_instance):
         self.classes[class_name] = class_instance
         return self.classes[class_name]
+
+    def get_host(self):
+        return environ.get('HOST', '0.0.0.0')
+
+    def get_port(self):
+        return environ.get('PORT', 8231)
+
+    def get_base_url(self):
+        return environ.get('BASE_URL', 'http://' + self.get_host() + ':' + str(self.get_port()))
