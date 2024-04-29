fa629be25c303c3335b497acf1ce98a9cbb06656
Michele Capichioni <capimichi@gmail.com>
2024-04-29 21:34:13 +0200

    Update default port in DefaultContainer.py

--- youtuberssapi/container/DefaultContainer.py
+++ youtuberssapi/container/DefaultContainer.py
@@ -39,7 +39,7 @@ class DefaultContainer:
         return environ.get('HOST', '0.0.0.0')
 
     def get_port(self):
-        return environ.get('PORT', 8231)
+        return environ.get('PORT', 8232)
 
     def get_base_url(self):
         return environ.get('BASE_URL', 'http://' + self.get_host() + ':' + str(self.get_port()))
