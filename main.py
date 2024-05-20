5bc993ba2ee193774662eb0255b4db3f523d1aa2
Michele Capichioni <capimichi@gmail.com>
2024-05-20 19:25:43 +0200

    Fixed bug with sorting algorithm, now works as expected.

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 from os import environ
 
 from fastapi import FastAPI
-
+import uvicorn
 from youtuberssapi.container.DefaultContainer import DefaultContainer
 from youtuberssapi.service.FeedService import FeedService
 import youtube_dl
 from fastapi.responses import Response
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+from fastapi import APIRouter
 
 from youtuberssapi.service.VideoService import VideoService
 
 default_container = DefaultContainer()
-app = FastAPI()
+app = FastAPI(docs_url="/swagger", servers=[{"url": default_container.get_base_url()}])
+router = APIRouter()
 
-app.mount("/", StaticFiles(directory="docusaurus/build", html=True), name="docusaurus")
 
-@app.get("/feed",tags=["feed"])
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url='/swagger')
+
+@router.get("/feed",tags=["Feed"])
 async def get_feed(channel_id: str, page: int = 1, per_page: int = 10):
     feed_service: FeedService = default_container.get('FeedService')
     feed_xml_data = feed_service.getFeed(channel_id, page=page, per_page=per_page)
     return Response(content=feed_xml_data, media_type="application/xml")
 
-@app.get("/video/{id}",tags=["video"])
+@router.get("/video/{id}",tags=["Video"])
 async def get_video(id: str):
     video_service: VideoService = default_container.get('VideoService')
     audio_content = video_service.getVideoMp3Content(id)
     return Response(content=audio_content, media_type="audio/mpeg")
 
+app.include_router(router, prefix="/api")
+
 # start the server
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host=default_container.get_host(), port=default_container.get_port())
