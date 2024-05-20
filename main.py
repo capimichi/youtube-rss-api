7b4000c8d7ee33b64f64ac37494cdeef1357737b
Michele Capichioni <capimichi@gmail.com>
2024-05-20 20:20:56 +0200

    Update root endpoint to redirect to /docs/ in main.py

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ default_container = DefaultContainer()
 app = FastAPI(docs_url="/swagger", servers=[{"url": default_container.get_base_url()}])
 router = APIRouter()
 
+# redirect / to /docs/
+@app.get("/", tags=["Root"], include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/docs/")
 
 app.mount("/docs/", StaticFiles(directory="./docusaurus/build", html=True), name="static")
 
