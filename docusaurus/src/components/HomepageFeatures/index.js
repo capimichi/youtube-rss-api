c198ae1dd6d5e1da3f64a15e870b169c3918046b
Michele Capichioni <capimichi@gmail.com>
2024-05-20 19:29:00 +0200

    "Update README to reflect Youtube RSS API changes"

--- docusaurus/src/components/HomepageFeatures/index.js
+++ docusaurus/src/components/HomepageFeatures/index.js
@@ -8,8 +8,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Youtube rss API is designed to be easy to use. You can use it to get
       </>
     ),
   },
@@ -18,8 +17,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Youtube rss API lets you focus on your content, and we'll do the chores.
       </>
     ),
   },
@@ -28,8 +26,7 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Youtube rss API is built using React, a popular JavaScript library for building user interfaces.
       </>
     ),
   },
